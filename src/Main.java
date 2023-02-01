9bf6c80317daf5b8d22609904b1453f1e8a43564
James Hayward <jamesjhayward99@gmail.com>
2023-02-01 16:36:31 +0000

    Had to use the src folder instead of root

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,78 @@
+// IMPORTS
+import java.util.Scanner;                                                               // Used to read input text
+
+/*
+NOTES:
+- na
+
+SPEC:
+- Login
+- - Database
+- - Password
+- - - Encryption
+- - - - Salting and peppering
+- - - - SHA?
+- - - - Maybe standard methods?
+
+- Balance
+- - Income
+- - - Annual taxes
+- - Outcomes
+- - - Deductables
+
+- Investment account
+- - Stock growth
+- - Dividend growth
+- - - Yield
+- - - Payout period
+- - - Annual yield
+- - - Result income
+- - - - 1, 2, 5, 10, 20, 40 years
+ */
+
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    // Variables
+    static String accountName;                                                          // Account name for login, raw input
+    static String accountPassword;                                                      // Account name for password, raw input
+
+    // CONSTANTS & ENUMs
+
+
+    // Functions
+
+
+    // Main
+    public static void main(String[] args){
+        System.out.println("Hello World!");                                             // Sanity check to see if the program works.
+
+        // Init
+        Scanner textInput = new Scanner(System.in);                                     // This is used for the scanner of the input
+
+
+        // Greeting
+        System.out.println("Welcome to my test banking app.");
+        System.out.println("--Made by J-Hayward99!");
+
+        // Login
+        System.out.println("Please login to your account.");
+
+        System.out.print("Login: ");
+        accountName = textInput.nextLine();                                             // Check docs to ensure no bad cases
+
+        System.out.print("Password: ");
+        accountPassword = textInput.nextLine();                                         // Check docs to ensure no bad cases
+
+        // # # INCLUDE REGEX
+
+        // Tests
+        System.out.println(
+                "\nYour login is:\t"
+                + accountName
+                + "\nYour password is:\t"
+                + accountPassword
+        );
+
     }
 }
